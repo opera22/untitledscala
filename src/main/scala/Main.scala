5aae59dcda3fdf4e45ae49b52b2e831f6b7893a9
Drew <drew@opera22.com>
2023-05-23 20:29:18 -0400

    update folder structure

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,7 @@
-package main
 import haha.*
 
 @main def hello(): Unit =
   val ha = Haha("Rose")
 //  println(ha.name)
   ha.speak()
-  println("this is the updated one")
+  println("this is the updated v2")
